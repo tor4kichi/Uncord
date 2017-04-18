f0507f4910c4b7c3f90ff52aadd8ca84b042acd2
tor4kichi <tor4kichi@hotmail.com>
2017-04-18 14:47:49 +0900

    #1 メッセージにアタッチされた画像表示を追加

---
+++ Uncord/Views/Controls/Message/MessageEmbedTypeTemplateSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uncord.Views.Controls.Message
+{
+    public sealed class MessageEmbedTypeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Image { get; set; }
+        public DataTemplate Video { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is string)
+            {
+                switch(item as string)
+                {
+                    case "image":
+                        break;
+                    case "video":
+                        break;
+                    default:
+                        throw new NotSupportedException();
+                }
+            }
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
